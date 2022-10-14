8c3f89e289d5667cacad158f7211ed25163bf6ef
taeyeon357 <teayun357@gmail.com>
2022-10-15 00:24:57 +0900

    디비 내용 채움, 텍스트 파일 참고

--- flask-server/db/csv2db.py
+++ flask-server/db/csv2db.py
@@ -1,30 +1,28 @@
+# -*- coding:utf-8 -*-
 import csv
 import sqlite3
 
 
 #database에 연결
-conn = sqlite3.connect('test.db')
+conn = sqlite3.connect('alryeoju.db')
 
 #cursor 객체를 만들고 실행
 cursor = conn.cursor()
 
-#테이블 정의
+#테이블 정의 - 테이블이 없을 경우
 #create_table = '''CREATE TABLE sool_(alname text, alid integer, img_url text);'''
     
-#db에 정의된 테이블 생성
+#db에 정의된 테이블 생성 - 테이블 생성
 #cursor.execute(create_table)
 
 #csv파일 오픈
-file = open('test.csv',encoding='utf-8-sig')
-#C:\Users\taeyeon\Desktop\asdfasdf\asdfasdf\test.csv
-#asdfasdf\test.csv
+file = open('buy_info.csv',encoding='utf-8-sig')
 contents = csv.reader(file)
 
-insert_records = 'INSERT INTO h1(name, id, img_url) values(?,?,?)'
-
+insert_records = 'INSERT INTO buy_info(al_id, u_id, datetime) values (?,?,?)'
 cursor.executemany(insert_records, contents)
 
-select_all = "SELECT * FROM h1"
+select_all = "SELECT * FROM buy_info"
 rows = cursor.execute(select_all).fetchall()
 
 for r in rows:
